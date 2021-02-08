f08413e12c52f4bddb95f63e0454bd002611e667
Lucas Nascimento <lucabelezal@gmail.com>
2021-02-08 17:00:48 -0300

    add ArticleActivity and its layout

---
+++ app/src/main/java/com/lucabelezal/newsapp/ArticleActivity.kt
@@ -0,0 +1,11 @@
+package com.lucabelezal.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+    }
+}
