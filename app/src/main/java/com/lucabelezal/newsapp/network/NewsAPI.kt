e69f6e8ff275baf47e55b6f666508f39a114c58f
Lucas Nascimento <lucabelezal@gmail.com>
2021-02-10 15:55:07 -0300

    add NewsAPI interface

---
+++ app/src/main/java/com/lucabelezal/newsapp/network/NewsAPI.kt
@@ -0,0 +1,30 @@
+package com.lucabelezal.newsapp.network
+
+import com.lucabelezal.newsapp.model.NewsResponse
+import com.lucabelezal.newsapp.utils.Enviorment.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+
+    @GET("/v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "br",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("/v2/everything")
+    suspend fun getSearchNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+}
